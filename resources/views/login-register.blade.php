0cb58984ba5c0cee64631aa7084c76ab2beeb2f3
Rehana-Kerubo <166706039+Rehana-Kerubo@users.noreply.github.com>
2025-04-29 10:12:26 +0300

    Login/register form draft

---
+++ resources/views/login-register.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login / Sign Up</title>
+
+  <!-- Link your CSS file -->
+  <!-- <link rel="stylesheet" href="login.css"> -->
+  <link rel="stylesheet" href="assets/css/login-register.css">
+
+  <!-- Add a nice Google Font -->
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+  <div class="container" id="container">
+
+    <!-- Sign Up Form -->
+    <div class="form-container sign-up-container">
+      <form action="#">
+        <h1>Create Account</h1>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+
+    <!-- Sign In Form -->
+    <div class="form-container sign-in-container">
+      <form action="#">
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Sign In</button>
+      </form>
+    </div>
+
+    <!-- Overlay Panels -->
+    <div class="overlay-container">
+      <div class="overlay">
+        <div class="overlay-panel overlay-left">
+          <h1>Welcome Back!</h1>
+          <p>To stay connected with us, please login with your personal info</p>
+          <button class="ghost" id="signIn">Sign In</button>
+        </div>
+        <div class="overlay-panel overlay-right">
+          <h1>Hello, Beautiful!</h1>
+          <p>Enter your details and start your journey with us!</p>
+          <button class="ghost" id="signUp">Sign Up</button>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Link your JavaScript file -->
+  <!-- <script src="login.js"></script> -->
+  <script src="assets/js/login-register.js"></script>      
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing');
 });
+Route::get('login', function () {
+    return view('login-register');
+});
 
 Route::get('/vendor/dashboard', function() {
     return view('/vendor/dashboard');
