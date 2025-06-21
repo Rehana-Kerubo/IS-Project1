ec16ea66e86836988038d2d37bc752aac59660f4
WMacharia <grace.macharia@strathmore.edu>
2025-06-21 17:22:01 +0300

    Admin login page

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,10 @@
+<form action="{{ route('admin.login') }}" method="POST">
+    @csrf
+    <label>Email:</label>
+    <input type="email" name="email" required>
+    
+    <label>Password:</label>
+    <input type="password" name="password" required>
+
+    <button type="submit">Login</button>
+</form>
