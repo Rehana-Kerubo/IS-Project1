5af55620cadca52074d9b60766932aa56dc70fd5
WMacharia <grace.macharia@strathmore.edu>
2025-06-22 18:46:58 +0300

    Announcement controller

---
+++ app/Http/Controllers/AnnouncementController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Announcement;
+
+class AnnouncementController extends Controller
+{
+    public function index() {
+        $announcements = Announcement::orderBy('start_date', 'desc')->get();
+        return view('admin.announcements', compact('announcements'));
+    }
+
+    public function create()
+    {
+        return view('admin.create-announcements');
+    }
+
+
+    public function store(Request $request) {
+        $request->validate([
+            'title' => 'required|string|max:100',
+            'description' => 'nullable|string',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+        ]);
+
+        Announcement::create($request->all());
+
+        return redirect()->route('admin.announcements.index')->with('status', 'Announcement posted successfully!');
+    }
+
+    public function destroy($id)
+    {
+        $announcement = Announcement::findOrFail($id);
+        $announcement->delete();
+
+        return redirect()->route('admin.announcements.index')->with('status', 'Announcement deleted successfully!');
+    }
+    public function edit($id)
+    {
+        $announcement = Announcement::findOrFail($id);
+        return view('admin.edit-announcements', compact('announcement'));
+    }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|string|max:100',
+            'description' => 'nullable|string',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+        ]);
+
+        $announcement = Announcement::findOrFail($id);
+        $announcement->update($request->all());
+
+        return redirect()->route('admin.announcements.index')->with('status', 'Announcement updated successfully!');
+    }
+
+
+
+}
