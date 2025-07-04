55120375b6d40e7645b79545f76f174434d0c2dd
Rehana-Kerubo <166706039+Rehana-Kerubo@users.noreply.github.com>
2025-07-04 13:18:43 +0300

    changes to the table name

--- database/migrations/2025_07_02_181952_add_stall_number_column_to_stall-payments_table.php
+++ database/migrations/2025_07_02_181952_add_stall_number_column_to_stall-payments_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('stall-payments', function (Blueprint $table) {
+        Schema::table('stall_payments', function (Blueprint $table) {
             $table->integer('stall_number')->nullable()->after('status');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('stall-payments', function (Blueprint $table) {
+        Schema::table('stall_payments', function (Blueprint $table) {
             $table->dropColumn('stall_number');
         });
     }
